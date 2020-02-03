9a664387a413b0cdf31d76000a9e3b2e3c0e86a7
Caíque Lima <caique.ti.lima@outlook.com>
2020-02-03 01:46:05 -0300

    Agora vai

--- demo/src/main/java/com/example/demo/Service/DuplaService.java
+++ demo/src/main/java/com/example/demo/Service/DuplaService.java
@@ -2,6 +2,7 @@ package com.example.demo.Service;
 
 import com.example.demo.DTO.DuplaDTO;
 import com.example.demo.RodizioUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -14,6 +15,7 @@ public class DuplaService {
     private final String LOG_DUPLAS = System.getProperty("user.dir").concat("/src/main/java/com/example/demo/File/log_duplas.txt");
     private final String PIPE = "\\|";
     private final String MEMBRO_LOCKADO = "*";
+    private final String MEMBROS = System.getProperty("user.dir").concat("/src/main/java/com/example/demo/File/membros.txt");
 
     private final MembroService membroService;
 
@@ -65,13 +67,13 @@ public class DuplaService {
                 .filter(membro -> !membrosLockados.contains(membro)).collect(Collectors.toList());
 
         List<String> novasDuplas = new ArrayList<>();
-
-        this.validarDuplas(membrosLockados, membrosRotativos, novasDuplas);
+        List<String> membrosAtualizados = new ArrayList<>();
+        this.validarDuplas(membrosLockados, membrosRotativos, novasDuplas, membrosAtualizados);
 
         return this.lerArquivoDeDuplas();
     }
 
-    private void validarDuplas(List<String> membrosLockados, List<String> membrosRotativos, List<String> novasDuplas) throws IOException {
+    private void validarDuplas(List<String> membrosLockados, List<String> membrosRotativos, List<String> novasDuplas, List<String> membrosAtualizados) throws IOException {
         List<String> duplas = this.lerArquivoDeDuplas();
 
         while (membrosLockados.size() + membrosRotativos.size() > 1) {
@@ -84,27 +86,32 @@ public class DuplaService {
 
             if (duplaJaMontada && membrosLockados.size() + membrosRotativos.size() == 2) {
                 this.construirDuplas();
-            }
-
-            if (duplaJaMontada) {
-                this.validarDuplas(membrosLockados, membrosRotativos, novasDuplas);
                 return;
             }
 
-//            List<String> membrosAtualizados = new ArrayList<>();
-//            if (StringUtils.isNotEmpty(idMembroRotativoSorteado)) {
-//                String membroRotativo = this.membroService.lerArquivoDeMembros().stream()
-//                        .filter(membro -> membro.split(PIPE)[0].equals(idMembroRotativoSorteado)).findFirst().get();
-//                membrosAtualizados.add(membroRotativo.concat("|*"));
-//
-//                String membroLockado = this.membroService.lerArquivoDeMembros().stream()
-//                        .filter(m -> m.split(PIPE)[0].equals(idMembroLockadoSorteado)).findFirst().get();
-//                membrosAtualizados.add(membroLockado.split(PIPE)[0].concat("|").concat(membroLockado.split(PIPE)[1]));
-//
-//                novasDuplas.add(idMembroRotativoSorteado.concat("|").concat(idMembroLockadoSorteado));
-//            }
+            if (duplaJaMontada) {
+                this.validarDuplas(membrosLockados, membrosRotativos, novasDuplas, membrosAtualizados);
+                return;
+            }
 
-            novasDuplas.add(idMembroLockadoSorteado.concat("|").concat(idMembroRotativoSorteado));
+            if (StringUtils.isNotEmpty(idMembroRotativoSorteado)) {
+                String membroRotativo = this.membroService.lerArquivoDeMembros().stream()
+                        .filter(membro -> membro.split(PIPE)[0].equals(idMembroRotativoSorteado)).findFirst().get();
+                membrosAtualizados.add(membroRotativo.concat("|*"));
+
+                String membroLockado = this.membroService.lerArquivoDeMembros().stream()
+                        .filter(m -> m.split(PIPE)[0].equals(idMembroLockadoSorteado)).findFirst().get();
+                membrosAtualizados.add(membroLockado.split(PIPE)[0].concat("|").concat(membroLockado.split(PIPE)[1]));
+
+                novasDuplas.add(idMembroRotativoSorteado.concat("|").concat(idMembroLockadoSorteado));
+            } else {
+                String membroLockado = this.membroService.lerArquivoDeMembros().stream()
+                        .filter(membro -> membro.split(PIPE)[0].equals(idMembroLockadoSorteado)).findFirst().get();
+                membrosAtualizados.add(membroLockado.split(PIPE)[0].concat("|").concat(membroLockado.split(PIPE)[1]).concat("|*"));
+
+                novasDuplas.add(idMembroLockadoSorteado.concat("|").concat(idMembroRotativoSorteado));
+            }
+
 
             if (membrosLockados.isEmpty()) {
                 membrosRotativos.removeIf(membro -> membro.split("\\|")[0].equals(idMembroLockadoSorteado));
@@ -123,18 +130,48 @@ public class DuplaService {
 
         if (!membrosLockados.isEmpty()) {
             novasDuplas.add(membrosLockados.get(0).split("\\|")[0].concat("|"));
+            membrosAtualizados.add(membrosLockados.get(0).split("\\|")[0].concat("|").concat(membrosLockados.get(0).split("\\|")[1]).concat("|*"));
             membrosLockados.clear();
         }
 
         if (!membrosRotativos.isEmpty()) {
             novasDuplas.add(membrosRotativos.get(0).split("\\|")[0].concat("|"));
+            membrosAtualizados.add(membrosRotativos.get(0).split("\\|")[0].concat("|").concat(membrosRotativos.get(0).split("\\|")[1]).concat("|*"));
             membrosRotativos.clear();
         }
 
         RodizioUtils.resetarArquivo(LOG_DUPLAS);
 
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(LOG_DUPLAS, true));
-        bufferedWriter.append(String.join(System.lineSeparator(), novasDuplas));
-        bufferedWriter.close();
+        BufferedWriter bufferedWriterLog = new BufferedWriter(new FileWriter(LOG_DUPLAS, true));
+        bufferedWriterLog.append(String.join(System.lineSeparator(), novasDuplas));
+        bufferedWriterLog.close();
+
+        RodizioUtils.resetarArquivo(MEMBROS);
+
+        membrosAtualizados.forEach(membro -> {
+            BufferedWriter bufferedWriter = null;
+            try {
+                bufferedWriter = new BufferedWriter(new FileWriter(MEMBROS, true));
+
+                int maxIdMembro = this.membroService.lerArquivoDeMembros().stream()
+                        .mapToInt(m -> Integer.valueOf(m.split("\\|")[0]))
+                        .max()
+                        .orElse(0);
+
+                String lineSeparator = maxIdMembro == 0 ? "" : System.lineSeparator();
+
+                bufferedWriter.append(
+                        lineSeparator
+                                .concat(Integer.toString(maxIdMembro + 1))
+                                .concat("|")
+                                .concat(membro.split("\\|")[1])
+                                .concat(membro.split("\\|").length > 2 ? "|*" : ""));
+                bufferedWriter.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
     }
 }
