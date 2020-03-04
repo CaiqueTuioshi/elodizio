1686c67f01705cb9e638a1ab6d25eef30c4c59f8
lgaricoix <lucasgaricoix@hotmail.com>
2020-03-03 22:20:08 -0300

    Melhorias no UI utilizando a biblioteca material-ui

--- web-elodizio/src/Elodizio.tsx
+++ web-elodizio/src/Elodizio.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import { buscarMembrosDTO, buscarDuplasDTO, save, iniciarDuplas, removerMembro, lockarDeslockarMembro, construirDuplas, zerarMembros, zerarDuplas } from './service/RodizioService';
 import { Membro } from './type/Membro';
-import {Row, Col, Container, FormGroup} from 'reactstrap'
+import { FormGroup } from 'reactstrap'
 
 import { Dupla } from './type/Dupla';
 import { FormikValues } from 'formik'
@@ -11,7 +11,7 @@ import MembroForm from './components/forms/MembroForm';
 import DuplaForm from './components/forms/DuplaForm';
 import MembrosList from './components/lists/MembrosList';
 import DuplasList from './components/lists/DuplasList';
-import { Button } from '@material-ui/core';
+import { Button, Container, Grid } from '@material-ui/core';
 
 type Props = {}
 
@@ -20,102 +20,110 @@ const Elodizio: React.FC<Props> = (props) => {
   const [duplasOld, setDuplasOld] = useState<Dupla[]>();
   const [duplasNew, setDuplasNew] = useState<Dupla[]>();
 
-useEffect(() => {
-  buscarMembrosDTO()
-  .then(response => setMembros(response.data))
-  .catch(error => console.log('error', error))
-}, [duplasNew])
+  useEffect(() => {
+    buscarMembrosDTO()
+      .then(response => setMembros(response.data))
+      .catch(error => console.log('error', error))
+  }, [duplasNew])
 
-useEffect(() => {
-  buscarDuplasDTO()
-  .then(response => setDuplasNew(response.data))
-  .catch(error => console.log('error', error))
-}, [])
+  useEffect(() => {
+    buscarDuplasDTO()
+      .then(response => setDuplasNew(response.data))
+      .catch(error => console.log('error', error))
+  }, [])
 
-const onMembroSubmit = (value: FormikValues) => {
-  save(value)
-  .then(response => setMembros(response.data))
-  .catch(error => console.log('error', error))
-}
+  const onMembroSubmit = (value: FormikValues) => {
+    save(value)
+      .then(response => setMembros(response.data))
+      .catch(error => console.log('error', error))
+  }
 
-const onDuplaSubmit = (value: FormikValues) => {
-  iniciarDuplas(value)
-  .then(response => setDuplasOld(response.data))
-  .catch(error => console.log('error', error))
-}
+  const onDuplaSubmit = (value: FormikValues) => {
+    iniciarDuplas(value)
+      .then(response => setDuplasOld(response.data))
+      .catch(error => console.log('error', error))
+  }
 
-const onLockarDeslockar = (idMembro: string) => {
-  lockarDeslockarMembro(idMembro)
-  .then(response => setMembros(response.data))
-  .catch(error => console.log('error', error))
-}
+  const onLockarDeslockar = (idMembro: string) => {
+    lockarDeslockarMembro(idMembro)
+      .then(response => setMembros(response.data))
+      .catch(error => console.log('error', error))
+  }
 
-const onConstruirDuplas = () => {
-  construirDuplas()
-  .then(response => {setDuplasOld(duplasNew); setDuplasNew(response.data)})
-  .catch(error => console.log('error', error))
-}
+  const onConstruirDuplas = () => {
+    construirDuplas()
+      .then(response => { setDuplasOld(duplasNew); setDuplasNew(response.data) })
+      .catch(error => console.log('error', error))
+  }
 
-const onZerarMembros = () => {
-  zerarMembros()
-  .then(() => setMembros([]))
-  .catch(error => console.log('error', error))
-}
+  const onZerarMembros = () => {
+    zerarMembros()
+      .then(() => setMembros([]))
+      .catch(error => console.log('error', error))
+  }
 
-const onZerarDuplas = () => {
-  zerarDuplas()
-  .then(() => {setDuplasOld([]); setDuplasNew([])})
-  .catch(error => console.log('error', error))
-}
+  const onZerarDuplas = () => {
+    zerarDuplas()
+      .then(() => { setDuplasOld([]); setDuplasNew([]) })
+      .catch(error => console.log('error', error))
+  }
 
-const onRemoveMembro = (id: string, index: number) => {
-  removerMembro(id)
-  .then(response => {
-    const updateMembros = [
-      ...membros ? membros.slice(0, index) : [],
-      ...membros ? membros.slice(index + 1) : []
-    ]
+  const onRemoveMembro = (id: string, index: number) => {
+    removerMembro(id)
+      .then(response => {
+        const updateMembros = [
+          ...membros ? membros.slice(0, index) : [],
+          ...membros ? membros.slice(index + 1) : []
+        ]
 
-    setMembros(updateMembros);
-    setDuplasNew(response.data);
-  })
-  .catch(error => console.log('error', error))
-}
+        setMembros(updateMembros);
+        setDuplasNew(response.data);
+      })
+      .catch(error => console.log('error', error))
+  }
 
   return (
-    <Container>
-      <Row>
-        <FormGroup>
-          <h3>Inserir Membro</h3>
-          <MembroForm onMembroSubmit={onMembroSubmit}/>
-        </FormGroup>
-        
-      </Row>
-      <Row>
-      <FormGroup>
-        <h3>Inserir Duplas</h3>
-        <DuplaForm onDuplaSubmit={onDuplaSubmit}/>
-      </FormGroup>
-      </Row>
+    <Container maxWidth="lg">
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <FormGroup>
+            <h3>Inserir Membro</h3>
+            <MembroForm onMembroSubmit={onMembroSubmit} />
+          </FormGroup>
+        </Grid>
 
-      <br></br>
+        <Grid id="inserir-duplas" item xs={12}>
+          <FormGroup>
+            <h3>Inserir Duplas</h3>
+            <DuplaForm onDuplaSubmit={onDuplaSubmit} />
+          </FormGroup>
+        </Grid>
 
-      <Row>
-        <Button size='large' variant="contained" color='primary' onClick={() => onZerarMembros()}>ZERAR MEMBROS</Button>
-        <Button size='large' variant="contained" color='primary' onClick={() => onZerarDuplas()}>ZERAR DUPLAS</Button>
-        <Button size='large' variant="contained" color='primary' onClick={() => onConstruirDuplas()}>SORTEAR DUPLAS</Button>
-      </Row>
-        
-      <Row>
-        {membros && (
-          <MembrosList membros={membros} onRemoveMembro={onRemoveMembro} onLockarDeslockar={onLockarDeslockar}/>
-        )}
+        <Grid container spacing={3}>
+          <Grid item >
+            <Button size='large' variant="contained" color='primary' onClick={() => onZerarMembros()}>ZERAR MEMBROS</Button>
+          </Grid>
 
-        {duplasNew && (
-          <DuplasList duplasOld={duplasOld} duplasNew={duplasNew}/>
-        )}
+          <Grid item>
+            <Button size='large' variant="contained" color='primary' onClick={() => onZerarDuplas()}>ZERAR DUPLAS</Button>
+          </Grid>
 
-      </Row>
+          <Grid item >
+            <Button size='large' variant="contained" color='primary' onClick={() => onConstruirDuplas()}>SORTEAR DUPLAS</Button>
+          </Grid>
+        </Grid>
+
+        <Grid item xs={6}>
+          {membros && (
+            <MembrosList membros={membros} onRemoveMembro={onRemoveMembro} onLockarDeslockar={onLockarDeslockar} />
+          )}
+
+          {duplasNew && (
+            <DuplasList duplasOld={duplasOld} duplasNew={duplasNew} />
+          )}
+
+        </Grid>
+      </Grid>
     </Container>
   );
 }
--- web-elodizio/src/components/forms/MembroForm.tsx
+++ web-elodizio/src/components/forms/MembroForm.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Formik, FormikProps, FormikValues } from 'formik'
+import { TextField, Button, Grid } from '@material-ui/core';
+
+// import './styles/MembroForm.css'
+
 type Props = {
-  onMembroSubmit:(dupla: FormikValues) => void
+  onMembroSubmit: (dupla: FormikValues) => void
 }
 
 type MembroFormValue = {
@@ -13,28 +17,34 @@ const initialValues = {
 }
 
 const MembroForm: React.FC<Props> = (props) => {
-  const {onMembroSubmit} = props
+  const { onMembroSubmit } = props
 
   return (
     <Formik
-        initialValues={initialValues} 
-        onSubmit={onMembroSubmit}
-        render={(formProps: FormikProps<MembroFormValue>) => (
-          <>
-          <input 
-            type="text" 
-            name='membro' 
-            placeholder='Membro' 
-            onBlur={value => {              
-              formProps.setFieldValue('membro', value.target.value)
-              formProps.setFieldTouched('membro', true)
+      initialValues={initialValues}
+      onSubmit={onMembroSubmit}
+      render={(formProps: FormikProps<MembroFormValue>) => (
+        <Grid container spacing={3}>
+          <Grid item sm={2} xs={2}>
+            <TextField
+              type="text"
+              name='membro'
+              placeholder='Membro'
+              onBlur={value => {
+                formProps.setFieldValue('membro', value.target.value)
+                formProps.setFieldTouched('membro', true)
               }
-            }
-          />
-          <button onClick={formProps.submitForm}>Adicionar</button>
-          </>
-        )}
-      />
+              }
+            />
+          </Grid>
+
+          <Grid item sm={2} xs={2}>
+            <Button id="btn-adicionar-membro" variant="contained" onClick={formProps.submitForm}>Adicionar</Button>
+          </Grid>
+
+        </Grid>
+      )}
+    />
   )
 }
 
